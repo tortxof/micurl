fd0f7dc434b46c9fed8401b3c1697633507da83b
Daniel Jones <tortxof@gmail.com>
2016-08-28 14:29:33 -0400

    Use a router for /new. Use strict.

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const crypto = require('crypto');
-var express = require('express');
-var mongoose = require('mongoose');
+'use strict';
 
-var app = express();
+const crypto = require('crypto');
+const express = require('express');
+const newRouter = express.Router();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.set('app_url', (process.env.APP_URL || 'http://localhost:' + app.get('port')));
@@ -11,8 +15,9 @@ app.set('mongo_password', (process.env.MONGO_PASSWORD || null));
 app.set('mongo_server', (process.env.MONGO_SERVER || 'localhost:27017'));
 app.set('mongo_db', (process.env.MONGO_DB || 'micurl'));
 
+
 (function() {
-  var db_uri;
+  let db_uri;
   if (app.get('mongo_user') && app.get('mongo_password')) {
     db_uri = 'mongodb://' +
       app.get('mongo_user') + ':' + app.get('mongo_password') +
@@ -22,36 +27,51 @@ app.set('mongo_db', (process.env.MONGO_DB || 'micurl'));
   }
   mongoose.connect(db_uri);
 })();
-var Url = require('./models/url');
+const Url = require('./models/url');
 
 app.get('/', function(req, res) {
   res.send('Hello.');
 });
 
-app.get(/^\/new\/.*/, function(req, res) {
-  var url = new Url();
-  url.original_url = req.originalUrl.match(/^\/new\/(.*)/)[1];
+newRouter.use(bodyParser.urlencoded({ extended: false }));
+
+newRouter.use(function(req, res, next) {
+  const url = new Url();
+  if (req.method === 'GET') {
+    url.original_url = req.originalUrl.match(/^\/new\/(.*)/)[1];
+  } else if (req.method === 'POST') {
+    url.original_url = req.body.url;
+  } else {
+    res.status(404).send('Bad request method.');
+  }
   if (! /^(?:http|https):\/\/.+\..+/.test(url.original_url)) {
     res.status(400).json({error: 'Not a valid url.'});
-    return;
+  } else {
+    req.new_url = url;
+    next();
   }
-  url.slug = crypto.randomBytes(3).toString('base64')
+});
+
+newRouter.all('/*', function(req, res) {
+  req.new_url.slug = crypto.randomBytes(3).toString('base64')
     .replace(/\+/g, '-')
     .replace(/\//g, '_');
-  url.save(function(err) {
+  req.new_url.save(function(err) {
     if (err) {
       res.send(err);
     } else {
       res.json({
-        original_url: url.original_url,
-        short_url: app.get('app_url') + '/' + url.slug
+        original_url: req.new_url.original_url,
+        short_url: app.get('app_url') + '/' + req.new_url.slug
       });
     }
   });
 });
 
+app.use('/new', newRouter);
+
 app.get(/^\/(?:\w|\-){4}/, function(req, res) {
-  var slug = req.originalUrl.match(/^\/(.{4,})/)[1];
+  const slug = req.originalUrl.match(/^\/(.{4,})/)[1];
   console.log(slug);
   Url.findOne({slug: slug}, 'original_url', function(err, url) {
     if (err) {
