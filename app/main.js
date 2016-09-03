0c862cbe4c023d59d792fd5362669d05604ade9a
Daniel Jones <tortxof@gmail.com>
2016-09-03 01:22:46 -0400

    React working. Next, implement ajax call.

--- app/main.js
+++ app/main.js
@@ -1,37 +1,87 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+var React = require('react')
+var ReactDOM = require('react-dom')
 
-var test_data = [{"original_url":"https://www.example.com/","short_url":"http://localhost:5000/GL4s", "key": "GL4s"}];
+const test_data = {"originalUrl":"https://www.example.com/","shortUrl":"http://localhost:5000/GL4s"}
+
+const UrlContainer = React.createClass({
+  getInitialState: function() {
+    return {urls: []}
+  },
+  handleUrlSubmit: function(url) {
+    console.log('UrlContainer handleUrlSubmit url', url)
+    this.setState({
+      urls: this.state.urls.concat([{originalUrl: url.originalUrl, shortUrl: test_data.shortUrl}])
+    })
+    console.log('UrlContainer handleUrlSubmit this.state.urls', this.state.urls)
+  },
+  render: function() {
+    return (
+      <div>
+        <UrlForm onUrlSubmit={this.handleUrlSubmit} />
+        <UrlList urls={this.state.urls} />
+      </div>
+    )
+  }
+})
+
+const UrlForm = React.createClass({
+  getInitialState: function() {
+    return {originalUrl: ''}
+  },
+  handleOriginalUrlChange: function(e) {
+    this.setState({originalUrl: e.target.value})
+  },
+  handleSubmit: function(e) {
+    e.preventDefault()
+    console.log('submit handler')
+    const originalUrl = this.state.originalUrl.trim()
+    if (!originalUrl) {
+      return
+    }
+    this.props.onUrlSubmit({originalUrl: originalUrl})
+    this.setState({originalUrl: ''})
+  },
+  render: function() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type='url'
+          placeholder='https://www.example.com/'
+          value={this.state.originalUrl}
+          onChange={this.handleOriginalUrlChange}
+        />
+        <button type='submit'>Get Short URL</button>
+      </form>
+    )
+  }
+})
 
 var UrlList = React.createClass({
   render: function() {
-    var urlNodes = this.props.data.map(function(url) {
-      return (
-        <Url original_url={url.original_url} short_url={url.short_url} key={url.key} />
-      );
-    });
+    console.log('UrlList render func this.props.urls', this.props.urls)
+    var urlNodes = this.props.urls.map(url => (<Url originalUrl={url.originalUrl} shortUrl={url.shortUrl} />))
     return (
       <div className="url-list">
         {urlNodes}
       </div>
-    );
+    )
   }
-});
+})
 
 var Url = ({
-  original_url,
-  short_url
+  originalUrl,
+  shortUrl
 }) => {
   return (
     <div className="url">
-      <div>Original URL: <a href={original_url}>{original_url}</a></div>
-      <div>Short URL: <a href={short_url}>{short_url}</a></div>
+      <div>Original URL: <a href={originalUrl}>{originalUrl}</a></div>
+      <div>Short URL: <a href={shortUrl}>{shortUrl}</a></div>
     </div>
   )
 }
 
 
 ReactDOM.render(
-  <UrlList data={test_data} />,
+  <UrlContainer />,
   document.getElementById('urls')
-);
+)
