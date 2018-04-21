4f5517e544e067c244f3bd19ab6f99c31482a80a
Daniel Jones <tortxof@gmail.com>
2018-04-21 11:58:34 -0400

    Add .container and headings to App

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,13 @@ import UrlContainer from './UrlContainer';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AboutContainer />
-        <UrlContainer />
+      <div className="container">
+        <h1>{process.env.REACT_APP_NAME}</h1>
+        <h3>a url shortener</h3>
+        <div className="App">
+          <AboutContainer />
+          <UrlContainer />
+        </div>
       </div>
     );
   }
